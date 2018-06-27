f640c2b56eeb434ae32d5990b37a46eec940cde1
Hayden Betts <haydenbetts@gmail.com>
2018-06-27 18:54:45 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,11 +1,17 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
 
   GENRES = []
 
   def initialize(title)
     @title = title
+  end
+
+  # i think we make a custom genre writer that
+  def genre=(genre)
+    @genre = genre
+    << # add it to the class constant
   end
 
   def turn_page
